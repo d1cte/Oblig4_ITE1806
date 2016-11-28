5ce082c3206fbacea7d9e708afc7acb66ffff0e6
Benedicte Karlsen <bka049@post.uit.no>
2016-11-28 23:06:38 +0100

    Moved things from main to separate methods for better readability

--- Oblig4_Datakom_Sikkerhet/src/GenSig.java
+++ Oblig4_Datakom_Sikkerhet/src/GenSig.java
@@ -4,66 +4,76 @@ import java.security.*;
  * Har brukt eksempelkoden fra oracle.com
 */
 class GenSig {
-
+	static String keystoreName = "mykeystore";
+	static KeyStore keystore;
+	static char[] keystorePassword = {'p', 'a', 's', 's', 'o', 'r', 'd'};
+	
+	static String certificateAlias = "bk";
+	static java.security.cert.Certificate certificate;
+	
+	static PublicKey publicKey;
+	static PrivateKey privateKey;
 
     public static void main(String[] args) {
-    	String keystoreName = "mykeystore";
-    	char[] keystorePassword = {'p', 'a', 's', 's', 'o', 'r', 'd'};
+//    	String keystoreName = "mykeystore";
+//    	char[] keystorePassword = {'p', 'a', 's', 's', 'o', 'r', 'd'};
+    	
     	
         if (args.length != 1) {
             System.out.println("Usage: GenSig nameOfFileToSign");
         }
         else try {
-        	// keystore
-        	KeyStore keystore = KeyStore.getInstance("JKS");
-        	FileInputStream keystoreFis = new FileInputStream(keystoreName);
-        	BufferedInputStream keystoreBufin = new BufferedInputStream(keystoreFis);
-        	keystore.load(keystoreBufin, keystorePassword);
+        	importKeystore();
+        	setCertificateAndKeys();
+
+        	// Create Signature Object
+        	Signature rsa = Signature.getInstance("SHA256withRSA");
+        	rsa.initSign(privateKey);
         	
-        	PrivateKey privateKey = (PrivateKey) keystore.getKey("bk", keystorePassword);
-        	
-        	java.security.cert.Certificate certificate = keystore.getCertificate("bk");
-        	
-        	PublicKey publicKey = certificate.getPublicKey();
-        	
-        	
-        	// Signature
-        	Signature dsa = Signature.getInstance("SHA256withRSA");
-        	
-        	dsa.initSign(privateKey);
-        	
-        	// Supply the Signature Object the Data to be Signed
-        	FileInputStream fis = new FileInputStream(args[0]);
-        	BufferedInputStream bufin = new BufferedInputStream(fis);
-        	byte[] buffer = new byte[1024];
-        	int len;
-        	while ((len = bufin.read(buffer)) >= 0) {
-        		dsa.update(buffer, 0, len);
-        	};
-        	bufin.close();
-        	
-        	// Generate the Signature
-        	byte[] realSig = dsa.sign();
-        	
-        	/* save the signature in a file */
-        	FileOutputStream sigfos = new FileOutputStream("signature");
-        	sigfos.write(realSig);
-        	sigfos.close();
+        	byte[] realSignature = signDataAndGenerateSignature(args[0], rsa);
+
+        	saveSignatureToFile(realSignature, "BKs-Signature");
      	
-//        	/* save the public key in a file */
-//        	byte[] key = publicKey.getEncoded();
-//        	FileOutputStream keyfos = new FileOutputStream("bkPublicKey");
-//        	keyfos.write(key);
-//        	keyfos.close();
  
         } catch (Exception e) {
             System.err.println("Caught exception " + e.toString());
         }
     }
     
-
-        
-
-
+    public static void importKeystore() throws Exception {
+    	keystore = KeyStore.getInstance("JKS");
+    	FileInputStream keystoreFis = new FileInputStream(keystoreName);
+    	BufferedInputStream keystoreBufin = new BufferedInputStream(keystoreFis);
+    	keystore.load(keystoreBufin, keystorePassword);
+    }
     
+    public static void setCertificateAndKeys() throws Exception {
+    	privateKey = (PrivateKey) keystore.getKey(certificateAlias, keystorePassword);
+    	certificate = keystore.getCertificate(certificateAlias);
+    	publicKey = certificate.getPublicKey();
+    }
+    
+    public static byte[] signDataAndGenerateSignature(String dataToBeSigned, Signature sigAlgorithm) throws Exception {
+    	// Supply the Signature Object the Data to be Signed
+    	FileInputStream fis = new FileInputStream(dataToBeSigned);
+    	BufferedInputStream bufin = new BufferedInputStream(fis);
+    	byte[] buffer = new byte[1024];
+    	int len;
+    	while ((len = bufin.read(buffer)) >= 0) {
+    		sigAlgorithm.update(buffer, 0, len);
+    	};
+    	bufin.close();
+    	
+    	// Generate the Signature
+    	return sigAlgorithm.sign();
+    }
+    
+    public static void saveSignatureToFile(byte[] signature, String filename) throws Exception {
+		FileOutputStream sigfos = new FileOutputStream(filename);
+		sigfos.write(signature);
+		sigfos.close();
+    }
+    
+    
+        
 }
