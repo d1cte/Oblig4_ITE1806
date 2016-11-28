8dcbb9cee065956da890efc984826785d1c074dd
BK <benedictek1@gmail.com>
2016-11-28 12:22:46 +0100

    generate key pairs

--- Oblig4_Datakom_Sikkerhet/src/Gensig.java
+++ Oblig4_Datakom_Sikkerhet/src/Gensig.java
@@ -11,8 +11,17 @@ class GenSig {
             System.out.println("Usage: GenSig nameOfFileToSign");
         }
         else try {
-
-        // the rest of the code goes here
+        	// Create keypair generator
+        	KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+        	
+        	// Initialize keypair generator
+        	SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        	keyGen.initialize(1024, random);
+        	
+        	// Generate the pair of keys
+        	KeyPair pair = keyGen.generateKeyPair();
+        	PrivateKey priv = pair.getPrivate();
+        	PublicKey pub = pair.getPublic();
 
         } catch (Exception e) {
             System.err.println("Caught exception " + e.toString());
